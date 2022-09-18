c0d521e844fe769710f6dd57bf167e0f2f62949b
Oscar Peñafiel <klossp12@gmail.com>
2022-09-18 12:13:05 -0500

    Interfaz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API : "https://apiferreteriaeldescanso.jomatelapps.com/api",
-  URL : "https://apiferreteriaeldescanso.jomatelapps.com",
+  URL : "https://apiferreteriaeldescanso.jomatelapps.com/",
   dataRol : {
     1 : '/gerente',
     2 : '/administrador',
@@ -10,7 +10,5 @@ export const environment = {
     5 : '/shop'
   },
   CLIENT_ID_PAYPAL : 'Abwlwu10H-nHsHeXj-Ob5vkaho0PTybd97HrLuLp3sQ0mF1R6f3A0E8cnr3Z00uhv4rxix7ZT7ASbq6D',
+  secretKey: "wu7W6SFa*9yc4Z0Zv49DJ%4!ILb&#2&WI^HSr!^5AepaZEA#Rn",
 };
-
-//"http://127.0.0.1:8000/api"
-//https://apiferreteriaeldescanso.jomatelapps.com/
